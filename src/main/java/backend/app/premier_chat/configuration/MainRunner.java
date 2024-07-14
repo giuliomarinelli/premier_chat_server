7cb8480169e1917af4e3f2b3978702529966bfa7
giuliomarinelli <giulio.marinelli@icloud.com>
2024-07-14 13:55:18 +0200

    implementing jwt configuration

---
+++ src/main/java/backend/app/premier_chat/configuration/MainRunner.java
@@ -0,0 +1,16 @@
+package backend.app.premier_chat.configuration;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class MainRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+}
---
+++ src/main/java/backend/app/premier_chat/security/JwtUtils.java
@@ -0,0 +1,10 @@
+package backend.app.premier_chat.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+
+
+
+}
