5adb5c8656d85ec4512b1331a713114da0318c39
giuliomarinelli <giulio.marinelli@icloud.com>
2024-07-14 10:29:10 +0200

    first commit

---
+++ src/main/java/backend/app/premier_chat/PremierChatApplication.java
@@ -0,0 +1,13 @@
+package backend.app.premier_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/app/premier_chat/PremierChatApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.app.premier_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
