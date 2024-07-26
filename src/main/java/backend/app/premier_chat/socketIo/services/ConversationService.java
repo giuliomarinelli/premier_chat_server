6a266ed583b6bcecc548f25b98bda74e5bacbcc9
giuliomarinelli <giulio.marinelli@icloud.com>
2024-07-26 15:50:25 +0200

    gone on with conversation service

--- src/main/java/backend/app/premier_chat/socketIo/services/ConversationService.java
+++ src/main/java/backend/app/premier_chat/socketIo/services/ConversationService.java
@@ -2,12 +2,14 @@ package backend.app.premier_chat.socketIo.services;
 
 import backend.app.premier_chat.Models.entities.User;
 import backend.app.premier_chat.Models.mongo_db_documents.Conversation;
+import backend.app.premier_chat.exception_handling.BadRequestException;
 import backend.app.premier_chat.exception_handling.NotFoundException;
 import backend.app.premier_chat.repositories.jpa.UserRepository;
 import backend.app.premier_chat.repositories.mongo_db.ConversationRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -31,7 +33,6 @@ public class ConversationService {
         return conversationRepository.findByParticipantsExactly(userId1, userId2)
                 .hasElement()
                 .defaultIfEmpty(false);
-
     }
 
     public Mono<Conversation> findConversationBetweenTwoUsers(UUID userId1, UUID userId2) {
@@ -41,8 +42,6 @@ public class ConversationService {
                     if (!value) throw new NotFoundException("Conversation not found");
                     return conversationRepository.findByParticipantsExactly(userId1, userId2);
                 });
-
-
     }
 
     public Conversation.Participant createParticipant(UUID userId) {
@@ -52,6 +51,27 @@ public class ConversationService {
         );
 
         return new Conversation.Participant(userId, user.getUsername(), user.getFirstName(), user.getLastName());
+
+    }
+
+    public Mono<Conversation> createConversation(UUID userId1, UUID userId2) {
+
+        return isThereAConversationBetweenTwoUsers(userId1, userId2).flatMap(value -> {
+
+            if (value)
+                throw new BadRequestException("Conversation with users with id " + userId1 +
+                        " and " + userId2 + " already exists");
+
+            Conversation.Participant participant1 = createParticipant(userId1);
+            Conversation.Participant participant2 = createParticipant(userId2);
+
+            Conversation conversation = new Conversation(List.of(participant1, participant2));
+
+            conversationRepository.save(conversation);
+
+            return Mono.just(conversation);
+
+        });
 
     }
 
